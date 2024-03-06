a505bbde2a0451aa2b521f2ee1579edeb390bee1
hagenchung2 <hagen.chung@edu.sait.ca>
2024-03-05 20:45:12 -0700

    add studenttable.js

---
+++ app/components/StudentTable.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const StudentTable = () => {
+    return (
+    <table class="table-auto">
+        <thead>
+            <tr>
+                <th>Student Name</th>
+                <th>Date of Birth</th>
+                <th>Current Grade</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+    );
+  };
+
+  export default Footer;
